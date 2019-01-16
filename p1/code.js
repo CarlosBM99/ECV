b14e24eca4a9accffde6ea3c8efea3e4a1a2de51
Carlos Bertomeu <carlos.bertomeu01@estudiant.upf.edu>
2019-01-16 16:30:16 +0100

    improving

--- p1/code.js
+++ p1/code.js
@@ -3,11 +3,14 @@ var input = document.querySelector("input")
 var messages_container = document.querySelector("#messages")
 button.addEventListener('click', sendMessage)
 
-function sendMessage(){
+function sendMessage() {
     console.log('aaaa')
     var element = document.createElement('div');
     console.log(input.value)
     element.innerHTML = input.value
+    element.className = "me"
     messages_container.appendChild(element)
     input.value = ''
+    var elem = document.getElementById('messages');
+    elem.scrollTop = elem.scrollHeight;
 }
