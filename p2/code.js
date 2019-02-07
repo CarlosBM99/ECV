e071bf490ab518ba515dce56ee0df6a78073e5c4
Carlos Bertomeu <carlos.bertomeu01@estudiant.upf.edu>
2019-02-07 16:09:36 +0100

    login working

--- p2/code.js
+++ p2/code.js
@@ -1,315 +1,35 @@
-var sendButton = document.querySelector(".sendButton")
-var input = document.querySelector(".inputText")
-var bodyMessages = document.querySelector(".bodyMessages")
-//function to send a message when click
-sendButton.addEventListener('click', function() {
-    if(input.value !== ''){
-        sendMessage()
-    }
+var enterButton = document.querySelector(".buttonEnter")
+enterButton.addEventListener('click', function(){
+    enterChat()
 })
-var inapp = document.querySelector('.content')
-var header = document.querySelector('.header')
-var refreshRooms = document.querySelector('.refreshRooms')
-refreshRooms.addEventListener('click', showRooms)
-function showRooms() {
-    checkServer()
-}
-
-var server = new SillyClient();
-
-var messages = {}
-var logCreate = document.querySelector('#logCreate')
-logCreate.addEventListener('click', loginCreate)
-var logUserName = document.querySelector('#logUserName')
-var userName = document.querySelector('.user')
-var logRoomName = document.querySelector('#logRoomName')
-logRoomName.addEventListener('keydown', function (e) {
-    onKey(e, 'login')
+var inputUserName = document.querySelector(".logUserName")
+inputUserName.addEventListener('keydown', function(e){
+    onKey(e, 'username')
 })
-var login = document.querySelector('.login')
-var user_name = ''
-//here we choose which server we want to connect
-function conncectToServer(roomName, userName) {
-    server.connect("ecv-etic.upf.edu:9000", roomName);
-    //server.connect("tamats.com:55000", roomName);
-    //server.connect("localhost:55000", roomName);
-}
-server.on_ready = function () {
-    server.user_name = user_name
-    server.clients[server.user_id].name = user_name
-}
-//function for the login
-function loginCreate() {
-    if (logUserName.value !== '' && logRoomName.value !== '') {
-        user_name = logUserName.value
-        conncectToServer(logRoomName.value, logUserName.value)
-        userName.innerHTML = logUserName.value
-        inapp.style.display = 'flex'
-        header.style.display = 'flex'
-        login.style.display = 'none'
-    }
-}
-//we are connecteds to the server
-server.on_connect = function () {
-    console.log('Connected to Server!')
-    checkServer()
-};
-
-//canvas variables
-var canvas = document.querySelector('#drawCanvas');
-var rect = canvas.parentNode.getBoundingClientRect();
-var ctx = canvas.getContext('2d');
-var color = document.querySelector(':checked').getAttribute('data-color');
-ctx.strokeStyle = color;
-ctx.lineWidth = '3';
-ctx.lineCap = ctx.lineJoin = 'round';
-console.log(rect.width)
-console.log(rect.height)
-//choose the color
-document.getElementById('colorSwatch').addEventListener('click', function () {
-    color = document.querySelector(':checked').getAttribute('data-color');
-}, false);
-
-canvas.addEventListener('mousedown', startDraw, false);
-canvas.addEventListener('mousemove', draw, false);
-canvas.addEventListener('mouseup', endDraw, false);
-//create a flag
-var isActive = false;
-
-//array to collect coordinates
-var plots = [];
-function drawOnCanvas(color, plots) {
-    if (color === 'white') {
-        ctx.lineWidth = '10';
-    } else {
-        ctx.lineWidth = '3';
-    }
-    ctx.strokeStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(plots[0].x, plots[0].y);
-
-    for (var i = 1; i < plots.length; i++) {
-        ctx.lineTo(plots[i].x, plots[i].y);
-    }
-    ctx.stroke();
-}
-//to know when are we drawing or not we created this two functions
-function startDraw(e) {
-    isActive = true;
-}
-
-function endDraw(e) {
-    isActive = false;
-    sendMessage('canvas', { color: color, plots: plots })
-    //empty the array
-    plots = [];
-}
-//function to draw on the canvas
-function draw(e) {
-    if (!isActive) return;
-
-    //cross-browser canvas coordinates
-    var x = e.offsetX || e.layerX - canvas.offsetLeft;
-    var y = e.offsetY || e.layerY - canvas.offsetTop;
-
-    plots.push({ x: x, y: y });
-
-    drawOnCanvas(color, plots);
-}
-//load the draws on the canvas
-function drawFromStream(message) {
-    if (!plots) return;
-
-    ctx.beginPath();
-    drawOnCanvas(message.color, message.plots);
-}
-//we add the messages to allmessages
-server.on_message = function (user_id, message) {
-    if (JSON.parse(message).type === 'allmessages') {
-        //delete previous messages from another room
-        while (bodyMessages.firstChild) {
-            bodyMessages.removeChild(bodyMessages.firstChild);
-            ctx.fillStyle = "#fff";
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-        }
-        let roomname = server.room.name
-        messages[roomname] = JSON.stringify(message).msg
-        for (var i in JSON.parse(message).msg) {
-            recieveMessage(JSON.parse(message).msg[i])
-        }
-    } else {
-        recieveMessage(message)
-    }
-}
-checkRoomInfo()
-function checkRoomInfo() {
-    server.on_room_info = function (info) {
-        //to know which users are inside
-        var nameroom = document.querySelector('.roomName')
-        nameroom.innerHTML = info.name
-        var clients = document.querySelector('.numberRoomClients')
-        clients.innerText = info.clients.length
-    };
-}
-//refresh the information of the server
-function checkServer() {
-    server.getReport(function (report) {
-        var checkListrooms = document.querySelector('#listrooms')
-        var rooms = document.querySelector('.rooms')
-        if (checkListrooms) {
-            rooms.removeChild(checkListrooms)
-        }
-        var listrooms = document.createElement('div')
-        listrooms.setAttribute('id', 'listrooms')
-        for (var room in report.rooms) {
-            var divCont = document.createElement('div')
-            var divRoom = document.createElement('div')
-            divRoom.className = 'room'
-            var nameRoom = document.createElement('div')
-            var separator = document.createElement('div')
-            var numberUsers = document.createElement('div')
-            if (room !== '') {
-                nameRoom.innerHTML = room
-                separator.innerHTML = ': '
-                numberUsers.innerHTML = report.rooms[room]
-                nameRoom.addEventListener('click', enterRoom)
-                divRoom.appendChild(nameRoom)
-                divRoom.appendChild(separator)
-                divRoom.appendChild(numberUsers)
-                divCont.appendChild(divRoom)
-                listrooms.appendChild(divCont)
+function onKey(e, type){
+    if(e.which === 13){
+        if(type === 'username'){
+            if(inputUserName.value !== ''){
+                enterChat()
             }
         }
-        rooms.appendChild(listrooms)
+    }
+}
+var socket = null
+
+function enterChat(){
+    socket = new WebSocket("ws://" + 'localhost:1337/?name=' + inputUserName.value)
+    socket.onopen = function() {
+        console.log("Socket has been opened :D")
+        this.send("Hello server")
+    }
+    socket.addEventListener('close', function(e){
+        console.log("Socket has been closed: ", e)
     })
-}
-//steps that must be done when someone join the chat
-server.on_user_connected = function (user_id) {
-    checkServer()
-    checkRoomInfo()
-    var containerMessageUserConnected = document.createElement('div')
-    containerMessageUserConnected.className = 'containerMessageUserConnected'
-    var messageUserConnected = document.createElement('div')
-    messageUserConnected.className = 'messageUserConnected'
-    messageUserConnected.innerHTML = 'User connected: ' + 'user_' + user_id + '!!'
-    containerMessageUserConnected.appendChild(messageUserConnected)
-    bodyMessages.appendChild(containerMessageUserConnected)
-    var numberusers = document.querySelector('.numberRoomClients')
-    var num = parseInt(numberusers.innerHTML) + 1
-    numberusers.innerHTML = num
-    //send the messages to the new user
-    let message = { type: "allmessages", msg: messages[server.room.name], user: server.user_name }
-    server.sendMessage(message, user_id)
-    bodyMessages.scrollTop = bodyMessages.scrollHeight
-}
-//steps that must be done when someone left the chat
-server.on_user_disconnected = function (user_id) {
-    checkServer()
-    checkRoomInfo()
-    var containerMessageUserDisconnected = document.createElement('div')
-    containerMessageUserDisconnected.className ='containerMessageUserDisconnected'
-    var messageUserDisconnected = document.createElement('div')
-    messageUserDisconnected.className ='messageUserDisconnected'
-    messageUserDisconnected.innerHTML = 'User disconnected: ' + 'user_' + user_id + '!!'
-    containerMessageUserDisconnected.appendChild(messageUserDisconnected)
-    bodyMessages.appendChild(containerMessageUserDisconnected)
-    var numberusers = document.querySelector('.numberRoomClients')
-    var num = parseInt(numberusers.innerHTML) - 1
-    numberusers.innerHTML = num
-    // send the messages to the new user
-    bodyMessages.scrollTop = bodyMessages.scrollHeight
-}
-
-function enterRoom(event) {
-    let name = event.target.innerHTML
-    //check actual room
-    if (!(name === server.room.name)) {
-        conncectToServer(name)
+    socket.onmessage = function(msg){
+        console.log("Received: " + msg.data)
+    }
+    socket.onerror = function (err){
+        console.log("error: ", err)
     }
 }
-//steps that must be done when someone receive a message
-function recieveMessage(message) {
-    if (JSON.parse(message).type !== 'canvas') {
-        var bodyMessages = document.querySelector('.bodyMessages');
-        var containerMessageOther = document.createElement('div');
-        containerMessageOther.className = 'containerMessageOther'
-        var messageOther = document.createElement('div');
-        messageOther.className = 'messageOther'
-        var userMessageOther = document.createElement('div');
-        userMessageOther.className = 'userMessageOther'
-        userMessageOther.innerHTML = JSON.parse(message).user
-        var messageTextOther = document.createElement('div');
-        messageTextOther.className = 'messageTextOther'
-        messageTextOther.innerHTML = JSON.parse(message).msg
-        messageOther.appendChild(userMessageOther)
-        messageOther.appendChild(messageTextOther)
-        containerMessageOther.appendChild(messageOther)
-        bodyMessages.appendChild(containerMessageOther)
-
-        //var elem = document.getElementById('messages');
-        bodyMessages.scrollTop = bodyMessages.scrollHeight
-        let roomname = server.room.name
-        if (messages[roomname]) {
-            messages[roomname].push(message)
-        } else {
-            messages[roomname] = []
-            messages[roomname].push(message)
-        }
-    } else {
-        drawFromStream(JSON.parse(message).msg)
-    }
-
-}
-//steps that must be done when someone send a message
-function sendMessage(type, mes) {
-    if (type !== 'canvas') {
-        var bodyMessages = document.querySelector('.bodyMessages');
-        var containerMessageMe = document.createElement('div');
-        containerMessageMe.className = 'containerMessageMe'
-        var messageMe = document.createElement('div');
-        messageMe.className = 'messageMe'
-        var userMessageMe = document.createElement('div');
-        userMessageMe.className = 'userMessageMe'
-        userMessageMe.innerHTML = server.user_name
-        var messageTextMe = document.createElement('div');
-        messageTextMe.className = 'messageTextMe'
-        messageTextMe.innerHTML = input.value
-        messageMe.appendChild(userMessageMe)
-        messageMe.appendChild(messageTextMe)
-        containerMessageMe.appendChild(messageMe)
-        bodyMessages.appendChild(containerMessageMe)
-        let message = { type: "msg", msg: input.value, user: server.user_name }
-        server.sendMessage(message)
-        let roomname = server.room.name
-        if (messages[roomname]) {
-            messages[roomname].push(JSON.stringify(message))
-        } else {
-            messages[roomname] = []
-            messages[roomname].push(JSON.stringify(message))
-        }
-        input.value = ''
-        //var elem = document.getElementById('messages');
-        bodyMessages.scrollTop = bodyMessages.scrollHeight;
-    }
-    else {
-        let message = { type: type, msg: mes, user: server.user_name }
-        server.sendMessage(message)
-    }
-}
-input.addEventListener('keydown', function (e) {
-    onKey(e, 'inputme')
-})
-//enter will log us in and send messages
-function onKey(e, type) {
-    //Enter Key
-    if (e.which == 13) {
-        if (type === 'inputme') {
-            if(input.value !== ''){
-                sendMessage()
-            }
-        }
-        else if (type === 'login') {
-            loginCreate()
-        }
-    }
-} 
--- p2/server/server.js
+++ p2/server/server.js
@@ -1,17 +1,20 @@
+// Variables
+var users = [];
+var uid = 0;
 // Http Server
 var http = require('http');
 var url = require('url');
 
-var server = http.createServer( function(request, response) {
-	console.log("REQUEST: " + request.url );
-	var url_info = url.parse( request.url, true ); //all the request info is here
-	var pathname = url_info.pathname; //the address
-	var params = url_info.query; //the parameters
-	response.end("OK!"); //send a response
+var server = http.createServer(function (request, response) {
+    console.log("REQUEST: " + request.url);
+    var url_info = url.parse(request.url, true); //all the request info is here
+    var pathname = url_info.pathname; //the address
+    var params = url_info.query; //the parameters
+    response.end("OK!"); //send a response
 });
 var port = 1337
-server.listen(port, function() {
-	console.log(`Server ready on port ${port}!`);
+server.listen(port, function () {
+    console.log(`Server ready on port ${port}!`);
 });
 
 // Express
@@ -21,23 +24,29 @@ app.get('/', (req, res) => {
     res.send('Hello World')
     console.log('nice')
 })
-app.listen(port+1, () => console.log(`Example app listening on port ${port+1}!`))
+app.listen(port + 1, () => console.log(`Example app listening on port ${port + 1}!`))
 
 // WebSocket Server
 var WebSocketServer = require('websocket').server;
 var wsServer = new WebSocketServer({ // create the server
     httpServer: server //if we already have our HTTPServer in server variable...
 });
-wsServer.on('request', function(request) {
+wsServer.on('request', function (request) {
     var connection = request.accept(null, request.origin);
-	console.log("NEW WEBSOCKET USER!!!");
-    connection.on('message', function(message) {
+    var user = {}
+    console.log("NEW WEBSOCKET USER!!!");
+    console.log('CONNECTION-INFO:', request.resourceURL.query)
+    user = request.resourceURL.query
+    user.uid = uid
+    console.log(user)
+    uid++
+    connection.on('message', function (message) {
         if (message.type === 'utf8') {
-	console.log( "NEW MSG: " + message.utf8Data ); // process WebSocket message
+            console.log("NEW MSG: " + message.utf8Data); // process WebSocket message
         }
     });
 
-    connection.on('close', function(connection) {
-	    console.log("USER IS GONE");// close user connection
+    connection.on('close', function (connection) {
+        console.log("USER IS GONE");// close user connection
     });
 });
